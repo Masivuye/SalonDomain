21fdf53dd91143931a0023f1cd22c2aa85412664
Masivuye <masivuyebheme@gmail.com>
2019-04-10 19:05:50 +0200

    third commit

--- src/main/java/domain/Staff.java
+++ src/main/java/domain/Staff.java
@@ -1,4 +1,50 @@
 package domain;
 
+
+
 public class Staff {
+    private String name;
+    private int staffNumber;
+
+    private Booking booking;
+
+    private Staff(){}
+
+    private Staff(Builder builder){
+        this.name = builder.name;
+        this.staffNumber = builder.staffNumber;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStaffNumber() {
+        return staffNumber;
+    }
+    public static class Builder{
+        private String name;
+        private int staffNumber;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+        public Builder staffNumber(int staffNumber){
+            this.staffNumber = staffNumber;
+            return this;
+        }
+        public Staff build(){
+            return new Staff(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" +
+                "name='" + name + '\'' +
+                ", staffNumber=" + staffNumber +
+                '}';
+    }
 }
