bcca771d4ae842fb98a1b01d5674fecdef850ad9
Masivuye <masivuyebheme@gmail.com>
2019-04-10 19:50:24 +0200

    last commit

---
+++ src/test/java/factory/CustomerFactoryTest.java
@@ -0,0 +1,33 @@
+package factory;
+
+import domain.Customer;
+import domain.HairStyle;
+import domain.Manicure;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CustomerFactoryTest {
+    @Test
+    public void getCustomer() {
+       String name = "Masi Bheme";
+        Customer customer = CustomerFactory.getCustomer(name,1245);
+        Assert.assertEquals("Masivuye","Masi Bheme");
+        System.out.print(name);
+
+    }
+    @Test
+    public void getManicure() {
+        String[] colors = {"Red","Pink","Yellow"};
+        String[] types = {"Nails","massage","Make-Up"};
+        Manicure manicure = ManicureFactory.getManicure(colors,types);
+        //Assert.assertArrayEquals(3,3);//I am trying to test my Arrays
+
+    }
+    @Test
+    public void getHairStyle() {
+        String[] types = {"Bradding","HairCut"};
+        HairStyle hair = HairStyleFactory.getHairStyle(types,150.00);
+        Assert.assertEquals(150.00,150.00);
+        //Test failed trying to test double
+    }
+}
