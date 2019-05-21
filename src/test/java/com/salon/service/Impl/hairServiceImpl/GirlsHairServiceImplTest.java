8b81d8c51abc4b0c21002a0e10b56726d54ae359
Masivuye <masivuyebheme@gmail.com>
2019-05-21 22:34:09 +0200

    testing

--- src/test/java/com/salon/service/Impl/hairServiceImpl/GirlsHairServiceImplTest.java
+++ src/test/java/com/salon/service/Impl/hairServiceImpl/GirlsHairServiceImplTest.java
@@ -1,33 +1,66 @@
 package com.salon.service.Impl.hairServiceImpl;
 
+import com.salon.domain.hair.GirlsHair;
+import com.salon.factory.hairFactory.GirlsHairFactory;
+import com.salon.repositories.impl.hairImpl.GirlsHairRepositoryImpl;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class GirlsHairServiceImplTest {
+
+    String[] types = {"Weave,Brading"};
+    private GirlsHairRepositoryImpl repository;
+    private GirlsHair girlsHair;
 
     @Before
     public void setUp() throws Exception {
+        this.repository = (GirlsHairRepositoryImpl) GirlsHairRepositoryImpl.getRepository();
+        this.girlsHair = GirlsHairFactory.getGirlsHair(types,150.00);
     }
 
     @Test
     public void getAll() {
+        Set<GirlsHair> girlsHairs = this.repository.getAll();
     }
 
     @Test
     public void create() {
+        GirlsHair created = this.repository.create(this.girlsHair);
+        System.out.println("created = "+created);
+        assertNotNull(created);
+        assertSame(created,this.girlsHair);
     }
 
     @Test
     public void update() {
+        String[] types = {"Condrows,Hair Relaxer"};
+        GirlsHair updated = new GirlsHair.Builder().types(types).price(500.00).build();
+        System.out.println("to be updated = "+girlsHair.getPrice());
+        this.repository.update(updated);
+        System.out.println("Updated = "+updated);
+        assertEquals(500.00,updated.getPrice(),500.00);
+        getAll();
     }
 
     @Test
     public void delete() {
+        this.repository.delete(girlsHair.getTypes());
+        getAll();
     }
 
     @Test
     public void read() {
+        System.out.println("in reading = "+girlsHair.getTypes());
+        GirlsHair read = this.repository.read(girlsHair.getTypes());
+        System.out.println("in read , read = "+read);
+        getAll();
+        assertNotEquals(girlsHair,read);
     }
 }
